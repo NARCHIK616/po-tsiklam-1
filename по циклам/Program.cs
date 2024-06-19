936a42de88994d255f2a58adf03a9b7263bca984
narek <narek@DESKTOP-98JS365>
2024-06-19 18:10:15 +0300

    Добавьте файлы проекта.

---
+++ по циклам/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите сумму вклада:");
+        decimal depositAmount = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Введите количество месяцев:");
+        int months = Convert.ToInt32(Console.ReadLine());
+
+        decimal interestRate = 0.07M; // 7% начисления
+
+        for (int i = 0; i < months; i++)
+        {
+            depositAmount += depositAmount * interestRate;
+        }
+
+        Console.WriteLine($"Сумма вклада через {months} месяцев: {depositAmount}");
+    }
+}
